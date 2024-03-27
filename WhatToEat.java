37174432c46ca75e15d22a1e5171fc1e4099ffd4
Ramsey Louder <RamseyLouder@gmail.com>
2024-03-27 12:43:16 -0400

    second commit

--- WhatToEat.java
+++ WhatToEat.java
@@ -5,35 +5,44 @@ public class WhatToEat {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner scnr=new Scanner(System.in);
+int sodastock = 100;
+int chipstock = 40;
+int candystock = 60;
+int sodarestock = 40;
+int chiprestock = 20;
+int candyrestock = 40;
+int stock;
+int restock;
 
 	System.out.println("How many sodas have been sold today? 100 in stock");
-		int sodas = scnr.nextInt();
-		int sodaleft = 100 - sodas;
-	if (sodaleft >= 0) {	
-	System.out.println("There are " + sodaleft + " sodas left." ); 
+		stock = scnr.nextInt();
+		sodarestock = sodastock - stock;
+		
+	if (sodarestock >= 0) {	
+	System.out.println("There are " + sodarestock + " sodas left." ); 
 	}
-		if (sodas > 100) {
+		if (sodarestock > 100) {
 	System.out.println("Too high, don't adjust the stock");
 	} 
 	System.out.println("How many chips have been sold today? 40 are in stock.");
-		int chips = scnr.nextInt();
-		int chipsleft = 40 - chips;
-		if (chipsleft >=0) {
+		stock = scnr.nextInt();
+		chiprestock = chipstock - stock;
+		if (chiprestock >=0) {
 	
-			System.out.println("There are "+ chipsleft + " Chips left.");
+			System.out.println("There are "+ chiprestock + " Chips left.");
 		}
 		
-		if (chips > 40) {
+		if (chiprestock > 40) {
 	System.out.println("Too high. Don't restock");
 		}
 	System.out.println("How many candies have been sold today? 60 are in stock");
-			int candy = scnr.nextInt();
-			int candyleft = 60 - candy;
-			if (candyleft >=0) {
+			stock = scnr.nextInt();
+			candyrestock = candystock - stock;
+			if (candyrestock >=0) {
 	
-				System.out.println("There are " + candyleft + " candies left.");
+				System.out.println("There are " + candyrestock + " candies left.");
 			}
-			if (candy > 60) {
+			if (candyrestock > 60) {
 	System.out.println("Too high. Don't restock");
 				
 				
@@ -41,20 +50,21 @@ Scanner scnr=new Scanner(System.in);
 		}
 	System.out.println("Thank you for filling out the values. Here is what needs to be restocked");
 			
-	if (sodaleft <=40 && sodaleft >=0) {
+	if (sodarestock <= 60) {
 	System.out.println("Soda needs to be restocked.");
 	}
 			
-	if(chipsleft <=20 && sodaleft >=0) {
+	if(chiprestock <= 20) {
 	System.out.println("Chips need to be restocked.");
 	}
 			
-	if(candyleft <=40 && candyleft >=0) {
-	System.out.println ( "Candy need to be restocked.");
+	if(candyrestock <= 40) {
+	System.out.println ("Candy need to be restocked.");
 		}  
 	
 			
 	System.out.println("Goodbye!");
+	
 		
 		
 		
